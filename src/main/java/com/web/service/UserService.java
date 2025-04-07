c294d632a430175668eb86d7ac68c9f711705764
srikarbharadwaj25 <srikargbharadwaj123@gmail.com>
2025-04-07 10:35:17 +0530

    Replace with correct project

--- src/main/java/com/web/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.web.service;
-
-import com.web.model.User;
-import com.web.web.dto.UserRegestationDto;
-
-public interface UserService {
-    User save(UserRegestationDto regestationDto);
-}
